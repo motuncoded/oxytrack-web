fcd9924cc5dbec69d52b8f897677ac0c36ac13bc
Motunrayo Adeneye <motunrayadeneye@gmail.com>
2025-12-03 05:21:13 +0100

    feat: create the scan page

--- src/components/ScanEntry.jsx
+++ src/components/ScanEntry.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import ScanTab from "./ScanTab";
 
 const ScanEntry = () => {
   return (
     <div className="p-4 w-full">
       <h2 className="text-2xl font-semibold  mt-4">Quick Scan Entry </h2>
-      <div className="mt-8 flex justify-center items-center h-48 bg-white rounded-lg">
-        <div className="w-5 h-5 bg-[#0097b2]" />{" "}
+      <div className="mt-6 flex justify-around items-center  bg-white rounded-lg">
+        <ScanTab tabs="1" title="Cylinder ID" />
+        <ScanTab tabs="2" title="Location & Status" />
+        <ScanTab tabs="3" title="Details" />
       </div>
     </div>
   );
---
+++ src/components/ScanTab.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ScanTab = ({ tabs, title }) => {
+  return (
+    <div className="">
+      <div className='flex items-center space-x-2"'>
+        <div className="w-8 h-8 bg-[#0097b2] rounded-full flex justify-center items-center">
+          <span className="text-white text-sm">{tabs}</span>
+        </div>{" "}
+        <div className="bg-gray-300 w-20 h-1"></div>
+      </div>
+      <p className="text-sm my-2 text-gray-500">{title}</p>
+    </div>
+  );
+};
+
+export default ScanTab;
