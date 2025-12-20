4d8e9051a08019dfdb018e4250ae52b32e91ba74
Motunrayo Adeneye <motunrayadeneye@gmail.com>
2025-12-20 21:25:02 +0100

    create the scan dialog

--- src/components/ScanEntry.jsx
+++ src/components/ScanEntry.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 import ScanTab from "./ScanTab";
+const ScanDialog = () => {
+  return (
+    <div className="shadow-md bg-white rounded-lg p-6 mt-6 max-w-xl mx-auto">
+      <div className="flex justify-center flex-col">
+        <h2 className="mb-2">Cylinder ID</h2>
+        <div>
+          <label>
+            <input type="text" placeholder="Enter or scan cylinder ID" />
+          </label>
+        </div>
+
+        <button className=" mt-6 flex justify-center items-center space-x-2 bg-[#0097b2] text-white px-4 py-2 rounded-md ">
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 const ScanEntry = () => {
   return (
@@ -10,6 +28,7 @@ const ScanEntry = () => {
         <ScanTab tabs="2" title="Location & Status" />
         <ScanTab tabs="3" title="Details" />
       </div>
+      <ScanDialog />
     </div>
   );
 };
--- src/components/ScanTab.jsx
+++ src/components/ScanTab.jsx
@@ -7,7 +7,7 @@ const ScanTab = ({ tabs, title }) => {
         <div className="w-8 h-8 bg-[#0097b2] rounded-full flex justify-center items-center">
           <span className="text-white text-sm">{tabs}</span>
         </div>{" "}
-        <div className="bg-gray-300 w-20 h-1"></div>
+        <div className="bg-gray-300 w-20 h-1 mx-2 rounded-2xl"></div>
       </div>
       <p className="text-sm my-2 text-gray-500">{title}</p>
     </div>
